f1731965ea039bc176d5534f4190c04f9d6dd625
jsik <survivor2199@yahoo.com>
2015-06-16 22:50:02 -0700

    remove the divs

--- js/wheel.js
+++ js/wheel.js
@@ -251,29 +251,4 @@ var wheel = {
 		ctx.strokeStyle = '#000000';
 		ctx.stroke();
 	},
-}
-
-window.onload = function() {
-	wheel.init();
-
-	var segments = [
-		"TV Shows",
-		"Free Turn",
-		"Capitals",
-		"Lose Turn",
-		"Movies",
-		"Bankrupt",
-		"History",
-		"Player's Choice",
-		"Music",
-		"Opponents' Choice",
-		"Quotes",
-		"Spin Again"
-	];
-	wheel.segments = segments;
-	wheel.update();
-
-	setTimeout(function() {
-		window.scrollTo(0, 1);
-	}, 0);
-}
+};
